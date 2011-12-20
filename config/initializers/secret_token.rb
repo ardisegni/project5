7e828e75c2ea38c3e358537ce994e921d43090cc
Ariel Di Segni <ardisegni@gmail.com>
2011-12-19 21:44:23 -0500

    Created three models User Photo and Comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project5::Application.config.secret_token = 'f532aaf381e7e94ed019cc51c724e37399b8681ac79d758a0d7db236ffa8af3ad49b3e81cdc6f4f050cb64f597b049b87450e3efabf1ad118372deb73e8f46ec'
